bd61e22895668bdfd0a8418432ca665ac338c118
Marcosegm <marcos-gm14@hotmail.com>
2023-05-15 14:02:51 -0500

    haciendo cambios

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,61 +1,61 @@
 import "./Home.css"
-import { Videos } from "../Videos"
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 
 const Home = (props) => {
+
     const {datos, videoNuevo} = props
 
     const settings = {
         dots: true,
         infinite: false,
         speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         initialSlide: 0,
         responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
           }
-        ]
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
       };
     
     return <div>
     { videoNuevo.length > 0 &&
-       
         <section className="categorias">
             <h2 className="tituloCategoria" style={{backgroundColor : datos.color}}> {datos.titulo} </h2>
-            <div className="carousel">
+            <div className="contenedorVideos">
                 <Slider {...settings}>
-                    {
-                        videoNuevo.map( (video, index) => <Videos
-                            datos={datos}
-                            info={video}
-                            key={index}/>)
-                    }
+                    {videoNuevo.map( (video) =>
+                        <div className="card" >
+                            <a className="subcard" href={video.linkVideo}  target="_blank" rel="noopener noreferrer"><img className="imagen" src={video.linkImagen} alt="imagen" style={{borderColor: datos.color}}/></a>
+                        </div>
+                    )}
                 </Slider>
             </div>
         </section>
